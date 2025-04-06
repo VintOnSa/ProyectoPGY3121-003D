7d56fda41551b18c4a6e35c82f0e94402ea531d0
Vint <127328649+VintOnSa@users.noreply.github.com>
2025-04-06 19:52:15 -0400

    Update settings.py

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -125,3 +125,5 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://proyectopgy3121-003d-production.up.railway.app']
